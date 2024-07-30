b2e113eb4e9bad460551a10df3f332ba191ea6e9
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-07-30 17:31:23 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import random 
+
+
+values = {"One":1, "Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, 
+          "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
+
+
+class Card: 
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+        
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+        
+ace_of_spades = Card("Spades","Ace")
+
+
+print(ace_of_spades.value)
