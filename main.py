96a66c26f5f72058f41259b5013654a1d23b5b09
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-07-30 20:50:45 -0500

    deck function update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random 
 
-
+suits = ("Hearts", "Diamonds", "Spades", "Clubs")
 values = {"One":1, "Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, 
           "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
+ranks = ("Two", "Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
 
 
 class Card: 
@@ -17,7 +18,37 @@ class Card:
         return self.rank + " of " + self.suit
 
         
-ace_of_spades = Card("Spades","Ace")
+class Deck:
 
-#
-print(ace_of_spades.value)
+    def __init__(self):
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                #creating a card
+
+                created_card = Card(suit,rank)
+
+                self.all_cards.append(created_card)
+
+
+new_deck = Deck()
+
+for cards in new_deck.all_cards:
+    print(cards)
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+        
