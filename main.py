7370c690546ab4541653975ee073f404ceac4d08
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-08-02 16:49:44 -0500

    update 10:18

--- main.py
+++ main.py
@@ -60,7 +60,8 @@ class Deck:
         '''
 
 class Player:
-
+    '''
+    Creating the Player class with a name as a parameter  '''
 
     def __init__(self,name):
             
@@ -69,7 +70,7 @@ class Player:
 
 
     def remove_one(self):
-            
+        #Dropping the first item in the list
         return self.all_cards.pop(0)
             
 
@@ -77,20 +78,74 @@ class Player:
     def add_cards(self,new_cards):
 
         if type(new_cards) == type([]):
+            #Adding list with another list
             self.all_cards.extend(new_cards)
-
+            #Adding a single object 
         else: self.all_cards.append(new_cards)
 
 
     def __str__(self):
         return f"Player  {self.name} has {len(self.all_cards)}"
+    
+
+
+    #Game setup
+
+player_one = Player("player_one")
+player_two = Player("player_two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+    #Game star 
+round = 0
+game_on = True
+
+while game_on:
+    #Counting the rounds
+    round += 1
+
+    if len(player_one.all_cards == 0):
+        print(f"player one has won the game ")
+        game_on = False
+
+    if len(player_two.all_cards == 0):
+        print(f"player two has won the game ")
+        game_on = False
+
+
+    #Start a new round
+
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+    #While at war
+    at_war = True 
+    
+    while at_war:
+        if player_one_cards[-1].value > player_two_cards[-1]:
+            
+
+
+
+
+    
+
+
+
+    
         
 
-first_player = Player("Carlos")
 
-first_player.add_cards(["1","2"])
-first_player.remove_one()
-print(first_player)
+
 
 
 
