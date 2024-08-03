d2e30ab23f9419c565aa99e2ec189145a26c5e8e
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-08-03 18:23:49 -0500

    game completed

--- main.py
+++ main.py
@@ -109,30 +109,93 @@ while game_on:
     #Counting the rounds
     round += 1
 
-    if len(player_one.all_cards == 0):
-        print(f"player one has won the game ")
+    if len(player_one.all_cards) == 0:
+        print(f"player one out of cards\n ")
+        print("player two has won the game ")
         game_on = False
+        break
 
-    if len(player_two.all_cards == 0):
-        print(f"player two has won the game ")
+    if len(player_two.all_cards) == 0:
+        print(f"player two out of cards ")
+        print("player one has won the game")
         game_on = False
+        break
 
 
     #Start a new round
 
     player_one_cards = []
+    '''List for the cards on the table'''
     player_one_cards.append(player_one.remove_one())
+    ''' Add the card that is removing from index 0 from player_one list '''
     
 
     player_two_cards = []
     player_two_cards.append(player_two.remove_one())
 
-    #While at war
-    at_war = True 
     
+    at_war = True 
+    #-----------------------------------------------------------------------------------
     while at_war:
-        if player_one_cards[-1].value > player_two_cards[-1]:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            ''' If player 1 card are greater than player two '''
             
+            player_one.add_cards(player_two_cards)
+            player_one.add_cards(player_one_cards)
+            ''' Player one keeps the cards'''
+            
+            at_war = False
+            
+            ''' End the loop'''
+        
+        elif player_two_cards[-1].value > player_one_cards[-1].value:
+            
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            
+            at_war = False
+            
+        
+        else: 
+            print("at war")
+
+            if len(player_one.all_cards) < 5 :
+                print("Player two has won the game")
+            
+                game_on = False
+                break
+                
+
+            elif len(player_two.all_cards) < 5 :
+                 print("Player one has won the game")
+                 
+                 game_on = False
+                 break
+                 
+
+            else:
+                
+                for card in range(5):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
+
+
+
+    
+
+            
+
+            
+
+
+                
+
+
+
+
+
 
 
 
